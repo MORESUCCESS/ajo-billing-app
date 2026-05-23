9df9f2b2c60c35f2f23df90194f161dcc6ee6125
ms2dwrld <hammedomoniyi3@gmail.com>
2026-05-23 22:30:10 +0100

    modified the navbars on the mobile view

--- src/components/bottomNavigations.jsx
+++ src/components/bottomNavigations.jsx
@@ -4,23 +4,23 @@ import { useState } from "react";
 
 const BottomNavigations = ()=>{
 
-    const [activeTab, SetActiveTab] = useState("home")
+    const [active, setActive] = useState("home")
     return(
         <nav className="lg:hidden fixed bottom-0 w-full flex items-center justify-center gap-7 p-3 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)] bg-white">
             <span 
-                onClick={()=>SetActiveTab("home")}
+                onClick={()=>setActive("home")}
                 className="flex flex-col items-center justify-center">
                 <Home className="w-6 h-6"/>
                 <span className="text-xs text-gray-600">Home</span>
             </span>
             <span 
-                onClick={()=>SetActiveTab("wallet")}
+                onClick={()=>setActive("wallet")}
                 className="flex flex-col items-center justify-center">
                 <Wallet className="w-6 h-6"/>
                 <span className="text-xs text-gray-600">Wallet</span>
             </span>
             <span 
-                onClick={()=>SetActiveTab("bills")}
+                onClick={()=>setActive("bills")}
                 className="flex flex-col items-center justify-center">
                 <CreditCard className="w-6 h-6"/>
                 <span className="text-xs text-gray-600">Bills</span>
